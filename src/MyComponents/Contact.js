a4b5f9e8ac80b45e712cb8cdd3cacc4cb0041467
siddiquimohdhammad <sidhammad7292@gmail.com>
2023-02-05 17:42:39 +0530

    PWA

--- src/MyComponents/Contact.js
+++ src/MyComponents/Contact.js
@@ -5,13 +5,13 @@ import './CSS/Contact.css'
 function Contact() {
   return (
     <div className="m-5 p-5">
-    <div className='container border border-danger p-5 bg-success bg-gradient text-dark'>
+    <div className='container border border-danger p-5 bg-primary bg-gradient text-light'>
         
 
         <form className='m-15 p-10'>
   <div className="mb-3">
     <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <input type="email" required className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <div id="emailHelp" className="form-text text-light">We'll never share your email with anyone else.</div>
   </div>
   <div className="mb-3">
@@ -22,7 +22,7 @@ function Contact() {
     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
     <label className="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" className="btn btn-danger">Submit</button>
+  <button type="submit" className="btn btn-warning">Submit</button>
 </form>
        
 
